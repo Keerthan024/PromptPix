fecb23b15e1d2bc33fddf69a4cfd6ab09b1eac03
Keerthan A <keerthann004@gmail.com>
2025-06-22 00:18:18 +0530

    Updated

---
+++ client/src/components/Description.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Description = () => {
+  return (
+    <div>
+        <h1>Create AI Images</h1>
+        <p>Turn your imagination into visuals</p>
+    </div>
+  )
+}
+
+export default Description
