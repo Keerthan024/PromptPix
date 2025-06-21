52d156efae3cbc70c6fa9bb964e11991f7781edd
Keerthan A <keerthann004@gmail.com>
2025-06-22 01:06:38 +0530

    Updated

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { assets, testimonialsData } from "../assets/assets";
 
 const Testimonials = () => {
   return (
@@ -7,6 +8,34 @@ const Testimonials = () => {
         Customer testimonials
       </h1>
       <p className="text-gray-500 mb-12">What Our User Are Saying</p>
+
+      <div className="flex flex-wrap gap-6">
+        {testimonialsData.map((testimonail, index) => (
+          <div
+            key={index}
+            className="bg-white/20 p-12 rounded-lg shadow-md border w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all"
+          >
+            <div className="flex flex-col items-center">
+              <img
+                src={testimonail.image}
+                alt=""
+                className="rounded-full w-14"
+              />
+              <h2 className="text-xl font-semibold mt-3">{testimonail.name}</h2>
+              <p className="text-gray-500 mb-4">{testimonail.role}</p>
+              <div className="flex mb-4">
+                {Array(testimonail.stars)
+                  .fill()
+                  .map((item, index) => (
+                    <img key={index} src={assets.rating_star} alt="" />
+                  ))}
+              </div>
+
+              <p className="text-center text-sm text-gray-600">{testimonail.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
