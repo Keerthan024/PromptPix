16264bff1c6dd1f0aecdb33ede1d2f71fc9f5abc
Keerthan A <keerthann004@gmail.com>
2025-06-29 18:10:23 +0530

    Backend Updated

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
+import { motion } from "framer-motion";
 
 const Login = () => {
   const [state, setState] = useState("Login");
-  const {setShowLogin} = useContext(AppContext)
+  const { setShowLogin } = useContext(AppContext);
 
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -16,7 +17,13 @@ const Login = () => {
 
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
-      <form className="relative bg-white p-10 rounded-xl text-slate-500">
+      <motion.form
+        initial={{ opacity: 0.2, y: 50 }}
+        transition={{ duration: 0.3 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className="relative bg-white p-10 rounded-xl text-slate-500"
+      >
         <h1 className="text-center text-2xl text-neutral-700 font-medium">
           {state}
         </h1>
@@ -96,12 +103,13 @@ const Login = () => {
           </p>
         )}
 
-        <img onClick={()=>setShowLogin(false)}
+        <img
+          onClick={() => setShowLogin(false)}
           src={assets.cross_icon}
           alt=""
           className="absolute top-5 right-5 cursor-pointer"
         />
-      </form>
+      </motion.form>
     </div>
   );
 };
