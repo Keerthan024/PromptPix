b18c54ea7e3696a4cf416253534d08f1a2cecd34
Keerthan A <keerthann004@gmail.com>
2025-07-02 12:38:49 +0530

    Updated Backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 // Initialize Express
 const app = express();
+await connectDB()
 
 // Middlewares
 app.use(cors());
@@ -11,6 +14,7 @@ app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => res.send("API Working"));
+app.use('/api/user', userRouter)
 
 // Port
 const PORT = process.env.PORT || 4000;
